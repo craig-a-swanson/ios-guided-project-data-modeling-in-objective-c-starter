816a3abb4f292902fd2d0e3cd2714adbd3026751
Craig Swanson <cswanson02@me.com>
2020-04-13 22:30:10 -0500

    Completed Project
    Added implementation for Quake Results

---
+++ Quakes-Objc/QuakeResults.m
@@ -0,0 +1,44 @@
+//
+//  QuakeResults.m
+//  Quakes-Objc
+//
+//  Created by Craig Swanson on 4/13/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "QuakeResults.h"
+#import "Quake.h"
+
+@implementation QuakeResults
+
+- (instancetype)initWithQuakes:(NSArray<Quake *> *)quakes {
+    if (self = [super init]) {
+        _quakes = quakes.copy;
+    }
+    return self;
+}
+
+- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    NSArray *quakeDictionaries = [dictionary objectForKey:@"features"];
+    if (![quakeDictionaries isKindOfClass:[NSArray class]]) return nil;
+    
+    NSMutableArray *quakes = [[NSMutableArray alloc] initWithCapacity:quakeDictionaries.count];
+    
+    for (NSDictionary *quakeDictionary in quakeDictionaries) {
+        if (![quakeDictionary isKindOfClass:[NSDictionary class]]) continue;
+        
+        Quake *quake = [[Quake alloc] initWithDictionary:quakeDictionary];
+        
+        if (quake) {
+            [quakes addObject:quake];
+        } else {
+            // TODO: One of our "required" fields might be optional and we may need to debug this with real data
+            NSLog(@"Unable to parse quake dictionary: %@", quakeDictionary);
+        }
+    }
+    
+    return [self initWithQuakes:quakes];
+}
+
+
+@end
